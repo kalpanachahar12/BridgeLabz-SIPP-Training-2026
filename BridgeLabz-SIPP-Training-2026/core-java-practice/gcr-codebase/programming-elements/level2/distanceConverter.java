22cd424fa94a354df4708d077dcc42ae2961c9fc
kalpana chahar <kkchahar121@gmail.com>
2026-06-11 10:13:27 +0530

    [kalpana] Added: level2 programming element

---
+++ BridgeLabz-SIPP-Training-2026/core-java-practice/gcr-codebase/programming-elements/level2/distanceConverter.java
@@ -0,0 +1,16 @@
+
+package level2;
+import java.util.*;
+public class distanceConverter {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the distance in feet:");
+        double distanceInFeet=sc.nextDouble();
+        double feetPerYard=3;
+        double yardsPerMile=1760;
+        double distanceInYards=distanceInFeet/feetPerYard;
+        double distanceInMiles=distanceInYards/yardsPerMile;
+        System.out.println("The distance in yards is "+distanceInYards+" while the distance in miles is "+distanceInMiles);
+        sc.close();
+    }
+}
---
+++ BridgeLabz-SIPP-Training-2026/core-java-practice/gcr-codebase/programming-elements/level2/divisionDetails.java
@@ -0,0 +1,11 @@
+package level2;
+
+public class divisionDetails {
+    public static void main(String[] args){
+        int dividend=25;
+        int divisor=4;
+        int quotient=dividend/divisor;
+        int remainder=dividend%divisor;
+        System.out.println("The quotient of "+dividend+" divided by "+divisor+" is "+quotient+" and the remainder is "+remainder);
+    }
+}
